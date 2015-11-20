f9a16a131ff2a1801e0aa9d7dbba282547486e1d
Nicole Cordes <typo3@cordes.co>
2015-11-21 00:09:50 +0100

    [TASK] Add basic extension files

---
+++ ext_emconf.php
@@ -0,0 +1,40 @@
+<?php
+
+/***************************************************************
+ * Extension Manager/Repository config file for ext "integrity".
+ *
+ * Auto generated 21-11-2015 00:04
+ *
+ * Manual updates:
+ * Only the data in the array - everything else is removed by next
+ * writing. "version" and "dependencies" must not be touched!
+ ***************************************************************/
+
+$EM_CONF[$_EXTKEY] = array (
+  'title' => 'Extension Integrity Check',
+  'description' => 'Checks the changes of extension files',
+  'category' => 'be',
+  'state' => 'stable',
+  'uploadfolder' => 0,
+  'createDirs' => '',
+  'clearCacheOnLoad' => 0,
+  'author' => 'Nicole Cordes',
+  'author_email' => 'typo3@cordes.co',
+  'author_company' => 'CPS-IT GmbH',
+  'version' => '0.1.0',
+  'constraints' => 
+  array (
+    'depends' => 
+    array (
+      'typo3' => '6.2.0-7.99.99',
+    ),
+    'conflicts' => 
+    array (
+    ),
+    'suggests' => 
+    array (
+    ),
+  ),
+  '_md5_values_when_last_written' => '',
+);
+
