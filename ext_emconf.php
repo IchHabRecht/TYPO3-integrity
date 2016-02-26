9c3d1c5d69caaf7e8788551fa0978d78046345b7
Nicole Cordes <typo3@cordes.co>
2016-02-26 22:44:17 +0100

    [TASK] Add autoload information

--- ext_emconf.php
+++ ext_emconf.php
@@ -35,6 +35,11 @@ $EM_CONF[$_EXTKEY] = array (
     array (
     ),
   ),
+  'autoload' => array(
+      'psr-4' => array(
+          'IchHabRecht\\Integrity\\' => 'Classes',
+      ),
+  ),
   '_md5_values_when_last_written' => '',
 );
 
